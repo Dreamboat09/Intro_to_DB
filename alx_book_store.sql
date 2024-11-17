4a6c90035cc18feb97beba4722519f1ffe19af8b
Dreamboat <enechimdindu@gmail.com>
2024-11-17 17:01:02 +0100

    shema for book store

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -2,43 +2,43 @@ CREATE DATABASE IF NOT EXISTS alx_book_store;
 
 USE alx_book_store;
 
+CREATE TABLE Authors (
+    author_id PRIMARY KEY INT AUTO_INCREMENT,
+    author_name VARCHAR(215) NOT NULL
+    ); 
+
 CREATE TABLE Books (
-    book_id PRIMARY KEY INT AUTO_INCREMENT,
+    book_id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(130) NOT NULL,
     author_id INT NOT NULL,
     FOREIGN Key (author_id) REFERENCES Authors (author_id),
     price DOUBLE,
-    publication_date DATE,
+    publication_date DATE
     );
 
-CREATE TABLE Authors (
-    author_id PRIMARY KEY INT AUTO_INCREMENT,
-    author_name VARCHAR(215) NOT NULL,
-    ); 
-
 CREATE TABLE Customers (
-    customer_id PRIMARY KEY INT AUTO_INCREMENT,
+    customer_id INT AUTO_INCREMENT PRIMARY KEY,
     customer_name VARCHAR(215) NOT NULL,
     email VARCHAR(215) NOT NULL UNIQUE,
-    address TEXT,
+    address TEXT
     );
 
 CREATE TABLE Orders (
-    order_id PRIMARY KEY INT AUTO_INCREMENT,
+    order_id INT AUTO_INCREMENT PRIMARY KEY,
     customer_id INT NOT NULL, 
     FOREIGN KEY (customer_id) REFERENCES Customers(customer_id),
-    order_date DATE NOT NULL,
+    order_date DATE NOT NULL
     );
 
     
 
 CREATE TABLE Order_Details ( 
-    orderdetail_id PRIMARY KEY INT AUTO_INCREMENT,
+    orderdetail_id INT AUTO_INCREMENT PRIMARY KEY,
     order_id INT NOT NULL,
     book_id INT NOT NULL, 
     FOREIGN KEY (order_id) REFERENCES Orders(order_id), 
     FOREIGN KEY (book_id) REFERENCES Books(book_id),
-    quantity DOUBLE,
+    quantity DOUBLE
     );
 
     
