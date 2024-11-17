3e709adc3f6e8df3318fba74a1e2a3c04ece4222
Dreamboat <enechimdindu@gmail.com>
2024-11-17 18:00:34 +0100

    python script for mysql

---
+++ MySQLServer.py
@@ -0,0 +1,23 @@
+import mysql.connector
+
+try:
+    mydb = mysql.connector.connect (
+        host="localhost",
+        user="root",
+        passwd="dreamboat",
+        database="alx_book_store",
+        )
+    
+    mycursor = mydb.cursor()
+    
+    mycursor.execute(CREATE DATABASE IF NOT EXISTS alx_book_store)
+        print("Database 'alx_book_store' created successfully!")
+        
+    
+except:
+    print(error!!!)
+ 
+finally:
+    mycursor.close()
+    mydb.close
+
