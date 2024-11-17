c17b2bb244753a5abdb18ad42161bae2eb3bf74f
Dreamboat <enechimdindu@gmail.com>
2024-11-17 18:41:49 +0100

    python script for mysql

--- MySQLServer.py
+++ MySQLServer.py
@@ -10,12 +10,12 @@ try:
     
     mycursor = mydb.cursor()
     
-    mycursor.execute(CREATE DATABASE IF NOT EXISTS alx_book_store)
-        print("Database 'alx_book_store' created successfully!")
+    mycursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
+    print("Database 'alx_book_store' created successfully!")
         
     
 except mysql.connector.Error:
-    print(error!!!)
+    print('error!!!')
  
 finally:
     mycursor.close()
